5e486d82ac49f988b6267b3c66d9f00058c61ec7
Ambati Navaneeth <ambatinavaneeth9@gmail.com>
2026-03-12 15:41:23 +0530

    Update sliding_window.py

--- sliding_window.py
+++ sliding_window.py
@@ -17,7 +17,30 @@ class Solution:
             max_sum=max(max_sum,win_sum)
         return max_sum/k
 
+>>>Minimum Positive Sum Subarray 
+class Solution:
+    def minimumSumSubarray(self, nums: List[int], l: int, r: int) -> int: #Brut
+        n=len(nums)
+        valid=[]
+        valid_sum=float('+inf')
+        for i in range(n):
+            for j in range(i+1,n+1):
+                subarray=nums[i:j]
+                if len(subarray)>=l and len(subarray)<=r:
+                    valid.append(subarray)
 
+        min_sum=float('+inf')  
+        for j in range(len(valid)):
+            summ=sum(valid[j])
+            if summ>0:
+                min_sum=min(summ,min_sum)
+            else:
+                continue
+        return min_sum if min_sum!=float('+inf') else -1  
+                
+
+            
+            
 
 
 
