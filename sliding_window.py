67e6bf6afcf1630cab69430e82263e804c2b7dc8
Ambati Navaneeth <ambatinavaneeth9@gmail.com>
2026-03-12 13:52:04 +0530

    Update sliding_window.py

--- sliding_window.py
+++ sliding_window.py
@@ -7,3 +7,22 @@ class Solution:
             avg=summ/k
             max_avg=max(avg,max_avg)
         return max_avg
+        
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:  #Optimal
+        win_sum=sum(nums[:k])
+        max_sum=win_sum
+        for i in range(k,len(nums)):
+            win_sum+=nums[i]-nums[i-k]
+            max_sum=max(max_sum,win_sum)
+        return max_sum/k
+
+
+
+
+
+
+
+
+
+
