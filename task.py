f2c7a556106600494fb006eaa9d4cca6ef5973f9
Ambati Navaneeth <ambatinavaneeth9@gmail.com>
2026-04-07 13:33:14 +0530

    Implement Fizz Buzz for numbers 1 to 100
    Add Fizz Buzz implementation to task.py

--- task.py
+++ task.py
@@ -251,7 +251,17 @@ for num in nums:
         ans.append(i)
 print(ans)
 
-
+✅FIZZ BUZZ
+for i in range(1,101):
+    if i%3==0:
+        print("buzz")
+    elif i%5==0:
+        print("Fizz")
+    elif i%3==0 and i%5==0:
+        print("buzzfizz")
+    else:
+        print(i)
+    
   
     
     
